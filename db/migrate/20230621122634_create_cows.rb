9cfe4e5e4925c6657a17d2dbbfae1b0d9583a1fc
rinoccha <at0113@outlook.jp>
2023-06-24 14:22:05 +0900

    dbのnumberをintegerからbigintに変更

--- db/migrate/20230621122634_create_cows.rb
+++ db/migrate/20230621122634_create_cows.rb
@@ -1,7 +1,7 @@
 class CreateCows < ActiveRecord::Migration[6.0]
   def change
     create_table :cows do |t|
-      t.integer :number,          null:false
+      t.bigint :number,          null:false
       t.date :birth_day
       t.text :memo
       t.references :user,         null: false,foreign_key: true
