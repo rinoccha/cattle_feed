affc311573712a191432947bcd63bbbb1e4cb440
rinoccha <at0113@outlook.jp>
2023-06-28 20:17:57 +0900

    データベース作成、マイグレート

---
+++ db/migrate/20230628100700_create_cow_feeds.rb
@@ -0,0 +1,9 @@
+class CreateCowFeeds < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cow_feeds do |t|
+      t.references :cow,      null: false, foreign_key: true
+      t.references :feed,     null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/cow_feed_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CowFeedTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
