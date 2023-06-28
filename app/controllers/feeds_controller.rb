729bb11787e0ab45422063273bca8d527cd91717
rinoccha <at0113@outlook.jp>
2023-06-28 21:34:31 +0900

    コントローラー設定

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,4 +2,23 @@ class FeedsController < ApplicationController
   def index
     @feeds = Feed.all
   end
+
+  def new
+    @feed = Feed.new
+  end
+
+  def create
+    if Feed.create(feed_params)
+      redirect_to feeds_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  
+  def feed_params
+    params.require(:feed).permit(:name, :price, :volume).merge(user_id: current_user.id)
+  end
 end
