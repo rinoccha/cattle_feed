4a2c2e10c122f6a288c9abc31bc2fad06e3aa20a
rinoccha <at0113@outlook.jp>
2023-07-01 21:33:58 +0900

    中間テーブルのデータベースの変更

--- db/migrate/20230628100700_create_cow_feeds.rb
+++ db/migrate/20230628100700_create_cow_feeds.rb
@@ -3,6 +3,7 @@ class CreateCowFeeds < ActiveRecord::Migration[6.0]
     create_table :cow_feeds do |t|
       t.references :cow,      null: false, foreign_key: true
       t.references :feed,     null: false, foreign_key: true
+      t.integer :volume,      null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_28_100700) do
-
-  create_table "cow_feeds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "cow_id", null: false
-    t.bigint "feed_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["cow_id"], name: "index_cow_feeds_on_cow_id"
-    t.index ["feed_id"], name: "index_cow_feeds_on_feed_id"
-  end
+ActiveRecord::Schema.define(version: 2023_06_21_122634) do
 
   create_table "cows", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "numbers", null: false
@@ -29,16 +20,6 @@ ActiveRecord::Schema.define(version: 2023_06_28_100700) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_cows_on_user_id"
-  end
-
-  create_table "feeds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.integer "volume", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,8 +35,5 @@ ActiveRecord::Schema.define(version: 2023_06_28_100700) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "cow_feeds", "cows"
-  add_foreign_key "cow_feeds", "feeds"
   add_foreign_key "cows", "users"
-  add_foreign_key "feeds", "users"
 end
