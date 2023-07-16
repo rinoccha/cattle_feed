4a5c8277854ed66fce09603fe94bfbafc53698a5
rinoccha <at0113@outlook.jp>
2023-07-16 17:44:13 +0900

    中間に餌のリストが出る、ドラッグ&ドロップで入れ替えができるようにJSを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../feed_edit")
+require("../cow")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
